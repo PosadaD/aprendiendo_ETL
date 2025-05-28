b88ad5c91284ffdd5e1963ba1885c1d7e47452e9
DIEGO POSADA <dapiposada@gmail.com>
2025-05-27 21:13:40 -0600

    new file:   pandas/clientes_banco.csv 	new file:   pandas/datos_wikipedia.csv 	modified:   pandas/trrabajando_diferentes_formatos.py

--- pandas/trrabajando_diferentes_formatos.py
+++ pandas/trrabajando_diferentes_formatos.py
@@ -41,3 +41,67 @@ todos_los_datos = pd.concat([datos.drop('nutritions', axis=1), datos_normalizado
 
 print(todos_los_datos.head());
 
+
+# Al igual que Vanessa, tu desafío es obtener un DataFrame de la tabla que contiene la información del número de habitantes de cada país.
+
+url = 'https://es.wikipedia.org/wiki/Anexo:Pa%C3%ADses_y_territorios_dependientes_por_poblaci%C3%B3n'
+
+#obten la primera tabla de la url
+datos_wikipedia = pd.read_html(url)[0];
+
+print(datos_wikipedia);
+
+datos_wikipedia.to_csv('datos_wikipedia.csv', index=False);
+
+
+# Crear la base de datos local con la biblioteca SQLAlchemy.
+# Escribir los datos del archivo CSV en esta base de datos local.
+# Realizar tres actualizaciones en la base de datos:
+
+import sqlalchemy 
+from sqlalchemy import text, create_engine, inspect
+
+engine = create_engine('sqlite:///:memory:');
+
+datos_banco = pd.read_csv("pandas/clientes_banco.csv");
+
+print(datos_banco.head());
+
+#cargar datos a SQL local 
+datos_banco.to_sql('clientes', engine, index=False);
+
+# consultar si la tabla se agrego correctamente
+print(inspect(engine).get_table_names());
+
+
+# Actualizar el registro del cliente ID 6840104 cuyo rendimiento anual cambió a 300000.
+# Eliminar el registro de cliente ID 5008809, ya que esta persona ya no tiene cuenta en la institución financiera.
+# Crear un nuevo registro de cliente siguiendo las especificaciones a continuación:
+# ID_cliente: 6850985
+# Edad: 33
+# Grado_estudio: Doctorado
+# Estado_civil: Soltero
+# Tamaño_familia: 1
+# Categoria_de_renta: Empleado
+# Ocupacion: TI
+# Años_empleado: 2
+# Rendimiento_anual: 290000
+# Tiene_carro: 0
+# Vivienda: Casa/Departamento propio
+
+
+with engine.begin() as conn:
+    conn.execute(text("UPDATE clientes SET Rendimiento_anual = 300000 WHERE ID_Cliente = 6840104"));
+    conn.execute(text("DELETE FROM clientes WHERE ID_Cliente=5008809"));
+    conn.execute(text("INSERT INTO clientes(ID_cliente, Edad, Grado_estudio, Estado_civil, Tamaño_familia, Categoria_de_renta, Ocupacion, Años_empleado, Rendimiento_anual, Tiene_carro, Vivienda) VALUES(6850985, 33, 'Doctorado', 'Soltero', 1, 'Empleado', 'TI', 2, 290000, 0, 'Casa/Departamento propio')"))
+
+consulta = pd.read_sql(sql="SELECT * FROM clientes WHERE ID_Cliente=6840104", con=engine.connect());
+consulta2 = pd.read_sql(sql="SELECT * FROM clientes WHERE ID_Cliente=5008809", con=engine.connect());
+consulta3 = pd.read_sql(sql="SELECT * FROM clientes WHERE ID_Cliente=6850985", con=engine.connect());
+print(consulta, consulta2, consulta3);
+
+
+
+
+
+
